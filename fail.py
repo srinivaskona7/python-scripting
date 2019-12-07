c8097d0d63eb76eaee25c549b795cce75cf21b62
Ubuntu <ubuntu@ip-172-31-83-22.ec2.internal>
2019-12-07 07:20:16 +0000

    added terminal

--- fail.py
+++ fail.py
@@ -1,8 +1,12 @@
 f = open('inputFile.txt','r')
 p = open('passfile', 'w')
+q = open('fail-list','w')
 for line in f:
     line_split = line.split()
     if line_split[2] == 'P':
         p.write(line)
+    else: 
+        q.write(line)
 f.close()
 p.close()
+q.close()
